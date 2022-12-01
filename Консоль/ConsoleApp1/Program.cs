f35593be5505cb89d4607a45d06cc9f73d20f8d0
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-01 20:08:43 +0300

    Add files via upload

---
+++ Консоль/ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, P = 0, a = 0, b = 0, c = 0;
+            do { 
+                try
+                {
+                    Console.Write("Введите x1: ");
+                    x1 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Только цифры");
+                }
+            } while (true);
+            do
+            {
+                try
+                {
+                    Console.Write("Введите y1: ");
+                    y1 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Только цифры");
+                }
+            } while (true);
+            do
+            {
+                try
+                {
+                    Console.Write("Введите x2: ");
+                    x2 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Только цифры");
+                }
+            } while (true);
+            do
+            {
+                try
+                {
+                    Console.Write("Введите y2: ");
+                    y2 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Только цифры");
+                }
+            } while (true);
+            do
+            {
+                try
+                {
+                    Console.Write("Введите x3: ");
+                    x3 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Только цифры");
+                }
+            } while (true);
+            do {
+                try
+                {
+                    Console.Write("Введите y3: ");
+                    y3 = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Только цифры");
+                }
+            } while (true);
+
+
+            a = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            b = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
+            c = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+
+            P = a + b + c;
+
+            Console.WriteLine("Периметр треугольника: " + P);
+            Console.ReadKey();
+        }
+    }
+}
+ 
---
+++ Консоль/Лин. алг. 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лин.алг._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                char[] a;
+                char[] check;
+                do
+                {
+                    try
+                    {
+                        Console.Write("Введите двух значное число: ");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        string c = Convert.ToString(b);
+                        check = c.ToCharArray();
+                        if (check.Length <= 3)
+                        {
+                            if (check[0] == '-')
+                            {
+                                a = check;
+                                break;
+                            }
+                            else if(check.Length == 2)
+                            {
+                                a = check;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Не двухзначное");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Число не двухзначное");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Только цифры");
+                    }
+
+                } while (true);
+                if (a.Length ==3 && (int.Parse(a[1].ToString()) + int.Parse(a[2].ToString())) % 3 == 0) Console.WriteLine("кратно ");
+                else if(a.Length == 3) Console.WriteLine("не кратно ");
+                else if (a.Length==2 && (int.Parse(a[0].ToString()) + int.Parse(a[1].ToString()) % 3 == 3)) Console.WriteLine("кратно");
+                else if (a.Length == 2 ) Console.WriteLine("не кратно");
+            } while (true);
+        }
+    }
+}
